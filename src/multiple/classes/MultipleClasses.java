bf41c51ac530fb4dd2e5021c60a43c32146bfbc6
DMarques <DMarques@nyc1alggce1064.home>
2014-03-28 16:56:30 -0400

    objects and classes

---
+++ src/multiple/classes/MultipleClasses.java
@@ -0,0 +1,13 @@
+package multiple.classes;
+/**
+ *
+ * @author DMarques
+ */
+public class MultipleClasses {
+
+    public static void main(String[] args) {
+       classOne object = new classOne();
+       object.simpleMessage();
+    }
+    
+}
